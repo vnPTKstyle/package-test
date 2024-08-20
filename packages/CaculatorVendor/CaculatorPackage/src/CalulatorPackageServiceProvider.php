0012f9637827308ccecddb59bbbc54ec4a948258
Phạm Tuấn Kiệt <ptkit1.0@gmail.com>
2024-08-20 09:01:52 +0700

    commit update

--- packages/CaculatorVendor/CaculatorPackage/src/CalulatorPackageServiceProvider.php
+++ packages/CaculatorVendor/CaculatorPackage/src/CalulatorPackageServiceProvider.php
@@ -2,23 +2,23 @@
 
 namespace CaculatorVendor\CaculatorPackage;
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
 class CalulatorPackageServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/Routes/web.php');
+//        $this->loadRoutesFrom(__DIR__ . '/Routes/web.php');
 
         // Đăng ký view
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'caculatorpackage');
+//        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'caculatorpackage');
 
         // Đăng ký config
-        $this->mergeConfigFrom(__DIR__ . '/../config/yourpackage.php', 'caculatorpackage');
+//        $this->mergeConfigFrom(__DIR__ . '/../config/yourpackage.php', 'caculatorpackage');
 
         // Đăng ký middleware
-        $router = $this->app->make(Route::class);
+        $router = $this->app->make(Router::class);
         $router->aliasMiddleware('log.http.methods', \CaculatorVendor\CaculatorPackage\Http\Middleware\LogHttpMethods::class);
 
         // Merge cấu hình logging
@@ -27,16 +27,13 @@ class CalulatorPackageServiceProvider extends ServiceProvider
 
         config()->set('logging.channels', array_merge($loggingConfig, $customConfig['channels']));
 
-        $this->publishes([
-            __DIR__ . '/../config/caculatorpackage.php' => config_path('caculatorpackage.php'),
-        ]);
     }
 
     public function register()
     {
         // Đăng ký binding vào container
         $this->app->singleton('caculatorpackage', function ($app) {
-            return new CalulatorPackageServiceProvider::class;
+            return CalulatorPackageServiceProvider::class;
         });
     }
 }
